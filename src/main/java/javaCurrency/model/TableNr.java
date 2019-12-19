ed4ca17538db4cc9958bc1c075be8b675fdd09c5
Stanislaw <sta.glegola@gmail.com>
2019-12-20 00:36:44 +0100

    Model dodany

---
+++ src/main/java/javaCurrency/model/TableNr.java
@@ -0,0 +1,51 @@
+package javaCurrency.model;
+
+import java.time.LocalDate;
+
+public class TableNr {
+    private char table;
+    private int tableEncodedNumber;
+    private char controlLetter = 'z';
+    private LocalDate localDate;
+
+    public TableNr(char table, int tableEncodedNumber, char controlLetter, LocalDate localDate) {
+        this.table = table;
+        this.tableEncodedNumber = tableEncodedNumber;
+        this.controlLetter = controlLetter;
+        this.localDate = localDate;
+    }
+
+    public char getTable() {
+        return table;
+    }
+
+    public void setTable(char table) {
+        this.table = table;
+    }
+
+    public int getTableEncodedNumber() {
+        return tableEncodedNumber;
+    }
+
+    public void setTableEncodedNumber(int tableEncodedNumber) {
+        this.tableEncodedNumber = tableEncodedNumber;
+    }
+
+    public LocalDate getDate() {
+        return localDate;
+    }
+
+    public void setDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    @Override
+    public String toString() {
+        return "TableNr{" +
+                "table=" + table +
+                ", tableEncodedNumber=" + tableEncodedNumber +
+                ", controlLetter=" + controlLetter +
+                ", localDate=" + localDate +
+                '}';
+    }
+}
